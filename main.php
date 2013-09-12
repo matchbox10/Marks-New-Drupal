58282b163979b18d1ef823a477b9851db04aafaf
Mark Simpson <mark.simpson78@hotmail.co.uk>
2013-09-12 01:08:56 +0100

    initial commit

---
+++ main.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Main Prototype2</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<script src="scripts/jquery-1.4.2.min.js"></script>
+</head>
+
+<body>
+
+<div id="wrapper">
+<div id="inner-content">
+<div id="header">
+<div id="first-col">
+<div id="rand">
+<img src="carousel/Fire_Raccoon_Mario_03.png" style="" />
+</div>
+<div id="logo">
+  <img src="images/logo-small.png" width="206" height="183" /></div>
+<ul id="main-menu" >
+<li><a class="black" href="#">Home</a></li>
+<li><a class="black" href="#">Blogs</a></li>
+<li><a class="black" href="#">Coding</a></li>
+<li><a class="black" href="#">Contact</a></li>
+<li><a class="black" href="#">About</a></li>
+</ul>
+</div>
+<div id="header-content" class="border-rad border-shadow">
+<div id="latest-article">
+<div id="latest-image" class="border-rad border-shadow crazy-border">
+  </div>
+  <h2>This is a test</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut turpis sed lorem sagittis rutrum. Aliquam sed interdum elit, sit amet auctor ante. Duis mollis magna ac metus hendrerit tincidunt. Duis nec ante tincidunt, viverra purus in, bibendum magna. Pellentesque cursus bibendum euismod. Donec aliquet massa eu magna sodales, in tincidunt dui malesuada. Duis scelerisque odio vestibulum felis auctor, at tincidunt erat sollicitudin.</p>
+  </div>
+</div>
+</div>
+<div id="body-content">
+<div class="side-bar">
+<div id="block1" class="block paper-bg border-rad crazy-border border-shadow">
+<h4>Archive</h4>
+<ul>
+<li><a href="#">September (4)</a></li>
+<li><a href="#">August (23)</a></li>
+<li><a href="#">July (14)</a></li>
+</ul>
+<a href="#">More..</a>
+</div>
+</div>
+
+<div id="main-content">
+<div class="item border-shadow">
+<h3>The Main Article Title</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut turpis sed lorem sagittis rutrum. Aliquam sed interdum elit, sit amet auctor ante. Duis mollis magna ac metus hendrerit tincidunt. Duis nec ante tincidunt, viverra purus in, bibendum magna. Pellentesque cursus bibendum euismod. Donec aliquet massa eu magna sodales, in tincidunt dui malesuada. Duis scelerisque odio vestibulum felis auctor, at tincidunt erat sollicitudin. Proin id elit arcu. Vestibulum ut ante at mi ultrices hendrerit in sit amet purus. Vestibulum lacinia nisi non sodales semper. In hac habitasse platea dictumst.</p>
+<p>Donec a imperdiet libero. Aliquam hendrerit pharetra pharetra. Cras iaculis fermentum magna, in luctus risus tincidunt non. Nunc ornare, ipsum in sollicitudin tincidunt, dolor arcu tempor tortor, eget ultrices sapien felis et tellus. Maecenas gravida et lacus id fringilla. Mauris imperdiet orci et est vehicula mattis. Mauris non lacinia ligula. Proin nisi orci, consectetur vitae ligula ut, adipiscing aliquam felis. Proin nec neque leo. Donec hendrerit consectetur tortor, eget vestibulum lorem viverra in. Donec volutpat tristique enim, ut semper lectus interdum eget. Sed eget leo odio. Nulla dignissim nibh diam, ut fringilla augue venenatis a. Curabitur et risus tellus. Suspendisse nec viverra velit.</p>
+</div>
+<div class="item border-shadow">
+<h3>This is Article number 2</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut turpis sed lorem sagittis rutrum. Aliquam sed interdum elit, sit amet auctor ante. Duis mollis magna ac metus hendrerit tincidunt. Duis nec ante tincidunt, viverra purus in, bibendum magna. Pellentesque cursus bibendum euismod. Donec aliquet massa eu magna sodales, in tincidunt dui malesuada. Duis scelerisque odio vestibulum felis auctor, at tincidunt erat sollicitudin. Proin id elit arcu. Vestibulum ut ante at mi ultrices hendrerit in sit amet purus. Vestibulum lacinia nisi non sodales semper. In hac habitasse platea dictumst.</p>
+<p>Donec a imperdiet libero. Aliquam hendrerit pharetra pharetra. Cras iaculis fermentum magna, in luctus risus tincidunt non. Nunc ornare, ipsum in sollicitudin tincidunt, dolor arcu tempor tortor, eget ultrices sapien felis et tellus. Maecenas gravida et lacus id fringilla. Mauris imperdiet orci et est vehicula mattis. Mauris non lacinia ligula. Proin nisi orci, consectetur vitae ligula ut, adipiscing aliquam felis. Proin nec neque leo. Donec hendrerit consectetur tortor, eget vestibulum lorem viverra in. Donec volutpat tristique enim, ut semper lectus interdum eget. Sed eget leo odio. Nulla dignissim nibh diam, ut fringilla augue venenatis a. Curabitur et risus tellus. Suspendisse nec viverra velit.</p>
+</div>
+</div>
+
+</div>
+<div id="footer">
+
+
+</div>
+</div>
+
+</div>
+</body>
+</html>
